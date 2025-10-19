9375162abb94219fd9afb9ed87f185f71e3347d3
ayaxxchanz <aliya.genius@gmail.com>
2025-10-19 19:03:36 +0800

    Styling Components using Tailwind CSS

---
+++ section5/eazystore-ui/src/components/Home.jsx
@@ -0,0 +1,12 @@
+import PageHeading from "./PageHeading"
+
+export default function Home(){
+    return (
+        <div className="max-w-[1152px] mx-auto px-6 py-8">
+            <PageHeading title="Explore Eazy Stickers!">
+                Add a touch of creativity to your space with our wide range of fun and unique stickers. 
+                Perfect for any occasion!
+            </PageHeading>        
+        </div>
+    )
+}
